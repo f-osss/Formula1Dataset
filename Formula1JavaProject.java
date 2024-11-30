44e77b3db924c8d55c5b4a4d5eb1e62b832b8ef2
Faiza <salami.faiza15@gmail.com>
2024-11-29 20:29:23 -0600

    updated java file

--- Formula1JavaProject.java
+++ Formula1JavaProject.java
@@ -1,2 +1,45 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
 public class Formula1JavaProject {
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+        String fileName = "auth.cfg";
+        try {
+            FileInputStream configFile = new FileInputStream(fileName);
+            prop.load(configFile);
+            configFile.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Could not find config file.");
+            System.exit(1);
+        } catch (IOException ex) {
+            System.out.println("Error reading config file.");
+            System.exit(1);
+        }
+        String username = (prop.getProperty("username"));
+        String password = (prop.getProperty("password"));
+
+        if (username == null || password == null) {
+            System.out.println("Username or password not provided.");
+            System.exit(1);
+        }
+
+        String connectionUrl =
+                "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
+                        + "database=cs3380;"
+                        + "user=" + username + ";"
+                        + "password=" + password + ";"
+                        + "encrypt=false;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+    }
 }
