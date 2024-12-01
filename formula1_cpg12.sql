9a7dcc0bcb1f091e913c5885fd737ea9428bf980
Faiza <salami.faiza15@gmail.com>
2024-11-30 22:39:45 -0600

    Update formula1_cpg12.sql

--- formula1_cpg12.sql
+++ formula1_cpg12.sql
@@ -1,4 +1,5 @@
-use cs3380;
+use
+cs3380;
 
 drop table if exists circuit;
 drop table if exists city;
@@ -18,3 +19,63 @@ drop table if exists qualifyingRecord;
 drop table if exists laptime;
 drop table if exists records;
 drop table if exists completeLapTime;
+
+create table circuit(
+    circuitID  integer primary key IDENTITY(1,1),
+    cityID     integer references city (cityID),
+    circuitRef varchar(255)  not null,
+    name       text  not null,
+    long       decimal(9, 6) not null,
+    lat        decimal(9, 6) not null,
+    altitude   integer       not null
+);
+
+create table constructor(
+    constructorID integer primary key IDENTITY(1,1),
+    constructorRef varchar(255) not null,
+    name text not null,
+    nationality text not null
+);
+
+create table race(
+    raceID integer primary key IDENTITY(1,1),
+    year integer not null,
+    round integer not null,
+    circuitID integer references circuit(circuitID),
+    name text not null,
+    date date not null,
+    time time
+);
+
+create table compete(
+    raceID integer references race(raceID) on delete cascade,
+    driverID integer references driver(driverID) on delete cascade,
+    primary key (raceID, driverID)
+);
+
+create table sprintResult(
+    resultID integer primary key IDENTITY(1,1),
+    raceID integer references race(raceID) on delete cascade,
+    driverID integer references driver(driverID) on delete cascade,
+    constructorID integer references constructor(constructorID) on delete cascade ,
+    number integer not null,
+    grid integer not null,
+    position integer,
+    positionOrder integer not null,
+    points integer not null,
+    laps integer not null,
+    time varchar(100),
+    milliseconds integer,
+    fastestLap integer,
+    fastestLapTime time,
+    statusID integer references status(statusID)
+);
+
+create table driverStanding(
+    driverStandingID integer primary key IDENTITY(1,1),
+    raceID integer references race(raceID) on delete cascade,
+    driverID integer references driver(driverID) on delete cascade,
+    points decimal(5,2) not null,
+    position integer not null,
+    wins integer not null
+);
