8c0e4f3de3e10a3d98861d9bcf6e7b45e3b64666
Saida <saidasanon@gmail.com>
2024-12-01 14:44:09 -0600

    add saidas tables

--- formula1_cpg12.sql
+++ formula1_cpg12.sql
@@ -44,6 +44,15 @@ create table constructor(
     nationality text not null
 );
 
+-- saida
+CREATE TABLE DrivesFor (
+constructorID INT NOT NULL,
+driverID INT NOT NULL,
+PRIMARY KEY (constructorID, driverID),
+FOREIGN KEY (constructorID) REFERENCES Constructor(constructorID), -- Reference to Constructor table
+FOREIGN KEY (driverID) REFERENCES Driver(driverID)                 -- Reference to Driver table
+)
+
 create table race(
     raceID integer primary key IDENTITY(1,1),
     year integer not null,
@@ -53,6 +62,56 @@ create table race(
     date date not null,
     time time
 );
+
+-- saida
+CREATE TABLE Constructor_Results (
+constructorResultsID INT PRIMARY KEY IDENTITY(1,1),
+raceID INT NOT NULL,
+constructorID INT NOT NULL,
+points DECIMAL(10, 2) NOT NULL,
+status VARCHAR(255) NOT NULL,
+FOREIGN KEY (raceID) REFERENCES Race(raceID),     -- Reference to the Race table
+FOREIGN KEY (constructorID) REFERENCES Constructor(constructorID) -- Reference to the Constructor table
+);
+
+--saida
+CREATE TABLE ConstructorStanding (
+constructorStandingID INT PRIMARY KEY IDENTITY(1,1),
+raceID INT NOT NULL,
+wins INT NOT NULL,
+points DECIMAL(10, 2) NOT NULL,
+position INT NOT NULL,
+constructorID INT NOT NULL,                           -- ID of the constructor
+FOREIGN KEY (raceID) REFERENCES Race(raceID),         -- Reference to the Race table
+FOREIGN KEY (constructorID) REFERENCES Constructor(constructorID) -- Reference to the Constructor table
+);
+
+
+CREATE TABLE QualifyingRecord (
+qualifyID INT PRIMARY KEY IDENTITY(1,1),
+raceID INT NOT NULL,
+driverID INT NOT NULL,
+constructorID INT NOT NULL,
+number INT NOT NULL,
+position INT NOT NULL,
+q1 DECIMAL(10, 3) NOT NULL,
+q2 DECIMAL(10, 3) NULL,              -- Time for Q2 session (optional, NULL if not reached)
+q3 DECIMAL(10, 3) NULL,                -- Time for Q3 session (optional, NULL if not reached)
+FOREIGN KEY (raceID) REFERENCES Race(raceID),              -- Links to Race table
+FOREIGN KEY (driverID) REFERENCES Driver(driverID),        -- Links to Driver table
+FOREIGN KEY (constructorID) REFERENCES Constructor(constructorID) -- Links to Constructor table
+);
+
+
+CREATE TABLE CompletesLapTime (
+lapID INT NOT NULL,
+driverID INT NOT NULL,
+lapTime DECIMAL(10, 3) NOT NULL,
+PRIMARY KEY (lapID, driverID),    -- Composite primary key to ensure each driver’s lap is unique
+FOREIGN KEY (driverID) REFERENCES Driver(driverID) -- Reference to Driver table
+);
+
+
 
 create table compete(
     raceID integer references race(raceID) on delete cascade,
