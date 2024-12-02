2a86ff83d752a7a9b6f90b8d74f754804f28f171
6huka <147269937+6huka@users.noreply.github.com>
2024-12-02 10:29:21 -0600

    Update formula1_cpg12.sql
    added my name in drop if section

--- formula1_cpg12.sql
+++ formula1_cpg12.sql
@@ -5,19 +5,19 @@ drop table if exists circuit;
 drop table if exists city;
 drop table if exists constructor;
 drop table if exists drivesFor;
-drop table if exists driver;
+drop table if exists driver; --chuka
 drop table if exists race;
 drop table if exists compete;
-drop table if exists result;
+drop table if exists result; --chuka
 drop table if exists sprintResult;
 drop table if exists driverStanding;
 drop table if exists constructorResults;
 drop table if exists constructorStanding;
-drop table if exists pitstop;
-drop table if exists status;
+drop table if exists pitstop; --chuka
+drop table if exists status; --chuka
 drop table if exists qualifyingRecord;
-drop table if exists laptime;
-drop table if exists records;
+drop table if exists laptime; --chuka
+drop table if exists records; --chuka
 drop table if exists completeLapTime;
 
 create table circuit(
