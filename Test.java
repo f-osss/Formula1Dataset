e98b0251871ef1d30b8aaae4d011c940a7d52a14
Faiza <salami.faiza15@gmail.com>
2024-12-03 15:42:54 -0600

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,72 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class Test {
+    private static String connectionUrl;
+
+    static {
+        Properties prop = new Properties();
+        String fileName = "auth.cfg";
+        try (FileInputStream configFile = new FileInputStream(fileName)) {
+            prop.load(configFile);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Could not find config file.");
+            System.exit(1);
+        } catch (IOException ex) {
+            System.out.println("Error reading config file.");
+            System.exit(1);
+        }
+
+        String username = prop.getProperty("username");
+        String password = prop.getProperty("password");
+
+        if (username == null || password == null) {
+            System.out.println("Username or password not provided.");
+            System.exit(1);
+        }
+
+        connectionUrl = "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
+                + "database=cs3380;"
+                + "user=" + username + ";"
+                + "password=" + password + ";"
+                + "encrypt=false;"
+                + "trustServerCertificate=false;"
+                + "loginTimeout=30;";
+    }
+
+    public static void printTableContents(String tableName) {
+        String selectSql = "SELECT * FROM " + tableName;
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            // Print column names
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
+            }
+            System.out.println();
+
+            // Print rows
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(resultSet.getString(i) + "\t");
+                }
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        printTableContents("driver"); // Replace "driver" with your desired table name
+    }
+}
