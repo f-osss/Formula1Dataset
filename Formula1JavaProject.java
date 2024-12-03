eee27c938070ca8f20d259cc030e12cd67117e1a
Faiza <salami.faiza15@gmail.com>
2024-12-02 21:18:33 -0600

    q

--- Formula1JavaProject.java
+++ Formula1JavaProject.java
@@ -1,18 +1,26 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class Formula1JavaProject {
-    public static void main(String[] args) {
+    private String connectionUrl;
 
+    public static void main(String[] args) {
+        Formula1JavaProject project = new Formula1JavaProject();
+        project.loadConfig();
+    }
+
+    public void loadConfig() {
         Properties prop = new Properties();
         String fileName = "auth.cfg";
+
         try {
             FileInputStream configFile = new FileInputStream(fileName);
             prop.load(configFile);
@@ -24,25 +32,24 @@ public class Formula1JavaProject {
             System.out.println("Error reading config file.");
             System.exit(1);
         }
-        String username = (prop.getProperty("username"));
-        String password = (prop.getProperty("password"));
+
+        String username = prop.getProperty("username");
+        String password = prop.getProperty("password");
 
         if (username == null || password == null) {
             System.out.println("Username or password not provided.");
             System.exit(1);
         }
 
-        String connectionUrl =
-                "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
-                        + "database=cs3380;"
-                        + "user=" + username + ";"
-                        + "password=" + password + ";"
-                        + "encrypt=false;"
-                        + "trustServerCertificate=false;"
-                        + "loginTimeout=30;";
+        connectionUrl = "jdbc:sqlserver://uranium.cs.umanitoba.ca:1433;"
+                + "database=cs3380;"
+                + "user=" + username + ";"
+                + "password=" + password + ";"
+                + "encrypt=false;"
+                + "trustServerCertificate=false;"
+                + "loginTimeout=30;";
 
     }
 
-    //QUERIES
 
 }
