e774ae3be186b06d7560a2334abe8e8bbb77b596
Chuka <nwokikcw@myumanitoba.ca>
2024-12-07 12:02:32 -0600

    commented out lapTime()

--- Populate.java
+++ Populate.java
@@ -66,7 +66,7 @@ public class Populate {
 //        constructorStanding();
 //        Pitstop();
 //        qualifyingRecord();
-        LapTime();
+//        LapTime();
 //        drivesFor();
 //        compete();
 
